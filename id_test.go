482e3737612e2b5a783cc9f9e443249e22320757
Willian Pinheiro <willpinhaaa@gmail.com>
2025-05-30 15:47:47 -0300

    Update go.mod and go.sum to include additional dependencies; refactor id_test.go for proper package declaration and testing structure

--- id_test.go
+++ id_test.go
@@ -1,1 +1,24 @@
-package main_test
+package main
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestId(t *testing.T) {
+	year := 2025
+	month := time.May
+	day := 30
+	hour := 05
+	minute := 45
+	second := 0
+	nsecond := 0
+
+	id := Id{
+		time: time.Date(year, month, day, hour, minute, second, nsecond, time.UTC),
+	}
+
+	require.Equal(t, "20250530054500", id.Value())
+}
